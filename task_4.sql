88ae287882e902d3c0313defa2312c8bedd593a3
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-02 08:55:11 -0800

    ..

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,8 @@
 --task_4.sql
 --lists all attributes of table books of the currently selected database
 USE alx_book_store;
-SELECT * FROM books;
+SELECT
+COLUMN_NAME,
+COLUMN_TYPE,
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = alx_book_store AND TABLE_NAME = Books;
