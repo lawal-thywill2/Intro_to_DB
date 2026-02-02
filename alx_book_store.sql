ef4999d138c0974fcb97da4e3bf2c0607e650511
lawal thywill <lawalkanyinsola889@gmail.com>
2026-02-02 07:41:57 -0800

    ..

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -132,12 +132,12 @@ DROP TABLE IF EXISTS orders;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE Orders (
-  order_id int NOT NULL AUTO_INCREMENT,
-  customer_id int DEFAULT NULL,
-  order_date date DEFAULT NULL,
+  order_id INT NOT NULL AUTO_INCREMENT,
+  customer_id INT DEFAULT NULL,
+  order_date DATE DEFAULT NULL,
   PRIMARY KEY (order_id),
   KEY customer_id (customer_id),
-  CONSTRAINT orders_ibfk_1 FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+  CONSTRAINT orders_ibfk_1 FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
